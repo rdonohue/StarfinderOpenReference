6f0900f767e769f69bb40b7dc0758501a77374f5
Nicholas-Babcock <babcon@uw.edu>
2020-07-07 18:32:58 -0700

    Made OGLPage activity

--- app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/MainActivity.java
@@ -2,7 +2,9 @@ package edu.tacoma.uw.ryandon.starfinderopenreference;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void OGLButton(View view) {
+        Intent intent = new Intent(this, OGLPage.class);
+        startActivity(intent);
+    }
 }
+
---
+++ app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/OGLPage.java
@@ -0,0 +1,15 @@
+package edu.tacoma.uw.ryandon.starfinderopenreference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OGLPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_o_g_l_page);
+
+}
+}
