c00127a1fcb08729bfec42ad872c42437cae150c
Nicholas-Babcock <babcon@uw.edu>
2020-07-07 18:53:30 -0700

    this is a test commit through android studio

--- app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    //this is a test comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
