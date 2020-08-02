e9c392e66f5fc40bc80e12a27346e54ef86c4300
Nicholas-Babcock <babcon@uw.edu>
2020-08-01 21:15:15 -0700

    web services work, looking tohook up members database for login authentication

---
+++ app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/authenticate/LoginFragment.java
@@ -0,0 +1,78 @@
+package edu.tacoma.uw.ryandon.starfinderopenreference.authenticate;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import edu.tacoma.uw.ryandon.starfinderopenreference.R;
+
+
+public class LoginFragment extends Fragment {
+    private LoginFragmentListener mLoginFragmentListener;
+
+    public LoginFragment() {
+
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        getActivity().setTitle("Sign In");
+        mLoginFragmentListener = (LoginFragmentListener) getActivity();
+        final EditText emailText = view.findViewById(R.id.editTextUsername);
+        final EditText pwdText = view.findViewById(R.id.editTextPassword);
+
+        Button loginButton = view.findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String email = emailText.getText().toString();
+                String pwd = pwdText.getText().toString();
+                if(TextUtils.isEmpty(email) || !email.contains("@")) {
+                    Toast.makeText(v.getContext(),"Enter valid email address",
+                            Toast.LENGTH_SHORT)
+                            .show();
+                    emailText.requestFocus();
+                }
+                else if (TextUtils.isEmpty(pwd) || pwd.length() < 6) {
+                    Toast.makeText(v.getContext(),"Enter a valid password( atleast 6 characters)"
+                            ,Toast.LENGTH_SHORT)
+                            .show();
+                    pwdText.requestFocus();
+
+                }
+
+
+                mLoginFragmentListener.login(emailText.getText().toString(), pwdText.getText().toString());
+            }
+        });
+
+        return  view;
+    }
+
+
+    public static interface LoginFragmentListener {
+
+        public void login(String email, String pwd);
+    }
+
+}
---
+++ app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/authenticate/SignInActivity.java
@@ -0,0 +1,51 @@
+package edu.tacoma.uw.ryandon.starfinderopenreference.authenticate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import edu.tacoma.uw.ryandon.starfinderopenreference.MainActivity;
+import edu.tacoma.uw.ryandon.starfinderopenreference.R;
+
+public class SignInActivity extends AppCompatActivity implements LoginFragment.LoginFragmentListener {
+
+    private SharedPreferences mSharedPreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        mSharedPreferences = getSharedPreferences(getString(R.string.LOGIN_PREFS)
+                , Context.MODE_PRIVATE);
+
+        if(mSharedPreferences.getBoolean(getString(R.string.LOGGEDIN), false)){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.sign_in_fragment_container, new LoginFragment())
+                    .commit();
+        }
+        else{
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();;
+        }
+
+
+    }
+    @Override
+    public void login(String email, String pwd) {
+
+        // in here we want to check if the email and pwd match a member in our members database
+        //we should look courses to see how the sql values were analyzed in android studio.
+
+        mSharedPreferences
+                .edit()
+                .putBoolean(getString(R.string.LOGGEDIN), true)
+                .commit();
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
