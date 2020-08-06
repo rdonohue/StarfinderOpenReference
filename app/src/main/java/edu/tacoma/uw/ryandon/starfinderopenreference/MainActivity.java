914351e7a0c066592051e6304076c01683ed7a28
ryandon <ra.donohue23@gmail.com>
2020-08-05 19:37:44 -0700

    redid MainActivity layout

--- app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/MainActivity.java
@@ -7,14 +7,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+import java.util.logging.Level;
+
+import edu.tacoma.uw.ryandon.starfinderopenreference.filters.LevelFilterFragment;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private boolean isChecked() {
+        return false;
     }
 
     @Override
---
+++ app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/filters/LevelFilterFragment.java
@@ -0,0 +1,62 @@
+package edu.tacoma.uw.ryandon.starfinderopenreference.filters;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.tacoma.uw.ryandon.starfinderopenreference.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LevelFilterFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LevelFilterFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public LevelFilterFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LevelFilterFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LevelFilterFragment newInstance(String param1, String param2) {
+        LevelFilterFragment fragment = new LevelFilterFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_level_filter, container, false);
+    }
+}
