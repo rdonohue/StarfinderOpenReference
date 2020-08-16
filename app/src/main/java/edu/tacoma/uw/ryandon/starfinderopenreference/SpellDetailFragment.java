e4436f698787bd101a7493dc66785b45a47ea036
ryandon <ra.donohue23@gmail.com>
2020-08-15 17:11:31 -0700

    added spell master/detail flow

---
+++ app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/SpellDetailFragment.java
@@ -0,0 +1,73 @@
+package edu.tacoma.uw.ryandon.starfinderopenreference;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import edu.tacoma.uw.ryandon.starfinderopenreference.model.Spell;
+import edu.tacoma.uw.ryandon.starfinderopenreference.model.SpellContent;
+
+/**
+ * A fragment representing a single Spell detail screen.
+ * This fragment is either contained in a {@link SpellListActivity}
+ * in two-pane mode (on tablets) or a {@link SpellDetailActivity}
+ * on handsets.
+ */
+public class SpellDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private Spell mSpell;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public SpellDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mSpell = SpellContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mSpell.getSpellName());
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.spell_detail, container, false);
+
+        // Show the spell content as text in a TextView.
+        if (mSpell != null) {
+            ((TextView) rootView.findViewById(R.id.spell_detail)).setText(mSpell.getSpellDescription());
+        }
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/SpellListActivity.java
@@ -0,0 +1,221 @@
+package edu.tacoma.uw.ryandon.starfinderopenreference;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.tacoma.uw.ryandon.starfinderopenreference.authenticate.SignInActivity;
+import edu.tacoma.uw.ryandon.starfinderopenreference.model.Spell;
+import edu.tacoma.uw.ryandon.starfinderopenreference.model.SpellContent;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * An activity representing a list of Spells. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link SpellDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class SpellListActivity extends AppCompatActivity {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+    private List<Spell> mSpellList;
+    private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spell_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        if (findViewById(R.id.spell_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+
+        mRecyclerView = findViewById(R.id.spell_list);
+        assert mRecyclerView != null;
+        setupRecyclerView(mRecyclerView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        StringBuilder url = new StringBuilder(getString(R.string.spells));
+        new SpellsTask().execute(url.toString());
+        setupRecyclerView(mRecyclerView);
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        if (mSpellList != null)
+            recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, mSpellList, mTwoPane));
+    }
+
+    /**
+     * Simple set up for Recycler View
+     */
+    public static class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final SpellListActivity mParentActivity;
+        private final List<Spell> mValues;
+        private final boolean mTwoPane;
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Spell item = (Spell) view.getTag();
+                if (mTwoPane) {
+                    Bundle arguments = new Bundle();
+                    arguments.putSerializable(SpellDetailFragment.ARG_ITEM_ID, item.getSpellName());
+                    SpellDetailFragment fragment = new SpellDetailFragment();
+                    fragment.setArguments(arguments);
+                    mParentActivity.getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.spell_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, SpellDetailActivity.class);
+                    intent.putExtra(SpellDetailFragment.ARG_ITEM_ID, item);
+
+                    context.startActivity(intent);
+                }
+            }
+        };
+
+        SimpleItemRecyclerViewAdapter(SpellListActivity parent,
+                                      List<Spell> items,
+                                      boolean twoPane) {
+            mValues = items;
+            mParentActivity = parent;
+            mTwoPane = twoPane;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.spell_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mIdView.setText(mValues.get(position).getSpellName());
+            holder.mContentView.setText(mValues.get(position).getClassName() + " "
+                                        + mValues.get(position).getSpellLevel());
+
+            holder.itemView.setTag(mValues.get(position));
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            final TextView mIdView;
+            final TextView mContentView;
+
+            ViewHolder(View view) {
+                super(view);
+                mIdView = (TextView) view.findViewById(R.id.id_text);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+        }
+    }
+    private class SpellsTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+                    InputStream content = urlConnection.getInputStream();
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+                } catch (Exception e) {
+                    response = "Unable to download the list of spells, Reason: " + e.getMessage();
+                }
+                finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s.startsWith("Unable to")) {
+                Toast.makeText(getApplicationContext(), "Unable to download " + s, Toast.LENGTH_LONG).show();
+                return;
+            }
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                if (jsonObject.getBoolean("success")) {
+                    Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+                    Log.e("Check Async", "json got the stuff");
+                    mSpellList = Spell.parseSpellsJson(jsonObject.getString("spells"));
+                    if (!mSpellList.isEmpty()) {
+                        setupRecyclerView((RecyclerView) mRecyclerView);
+                    }
+                }
+            } catch (JSONException e) {
+               Toast.makeText(getApplicationContext(), "JSON Error: " + e.getMessage(), Toast.LENGTH_LONG);
+            }
+        }
+
+
+    }
+}
