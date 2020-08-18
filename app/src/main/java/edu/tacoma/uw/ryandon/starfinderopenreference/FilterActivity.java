a900146f759194888ecd7b55209f0cd1b7600fa0
Nicholas-Babcock <babcon@uw.edu>
2020-08-17 22:22:33 -0700

    content sharing made

--- app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/FilterActivity.java
+++ app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/FilterActivity.java
@@ -6,21 +6,16 @@ import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 
-<<<<<<< HEAD:app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/MainActivity.java
+
 
 import android.os.AsyncTask;
-=======
->>>>>>> ace2c3df3d5f2f564ed0cbe03632a353bdec06a7:app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/FilterActivity.java
+
 import android.os.Bundle;
 import android.view.Menu;
 
 import android.view.View;
 import android.widget.CheckBox;
-<<<<<<< HEAD:app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/MainActivity.java
 
-import android.widget.Toast;
-=======
->>>>>>> ace2c3df3d5f2f564ed0cbe03632a353bdec06a7:app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/FilterActivity.java
 
 import org.json.JSONObject;
 
@@ -33,73 +28,57 @@ import edu.tacoma.uw.ryandon.starfinderopenreference.model.MembersContent;
 
 
 
-public class MainActivity extends AppCompatActivity {
-
-
-    public static FragmentManager fragmentManager;
-
-
-    public JSONObject mSpellsJson;
 
 
 
 
 
 
-
-public class FilterActivity extends AppCompatActivity {
-
-
-    public static FragmentManager fragmentManager;
-
-    private CheckBox classMystic, classTechnomancer, spellLevel0, spellLevel1
-            , spellLevel2, spellLevel3, spellLevel4, spellLevel5, spellLevel6, schoolAbj
-            , schoolConj, schoolDiv, schoolEnch, schoolEvoc, schoolIllu, schoolNec
-            , schoolTran, rangePersonal, rangeTouch, rangeClose, rangeMedium
-            , rangeLong, rangePlanetary, rangeSystem, rangePlane, castStandard
-            , castMinute, castTenMinutes;
-    private boolean classMysticCheck, classTechnomancerCheck, spellLevel0Check, spellLevel1Check
-            , spellLevel2Check, spellLevel3Check, spellLevel4Check, spellLevel5Check, spellLevel6Check
-            , schoolAbjCheck, schoolConjCheck, schoolDivCheck, schoolEnchCheck, schoolEvocCheck
-            , schoolIlluCheck, schoolNecCheck, schoolTranCheck, rangePersonalCheck, rangeTouchCheck
-            , rangeCloseCheck, rangeMediumCheck, rangeLongCheck, rangePlanetaryCheck, rangeSystemCheck
-            , rangePlaneCheck, castStandardCheck, castMinuteCheck, castTenMinutesCheck;
+    public class FilterActivity extends AppCompatActivity {
+        public static FragmentManager fragmentManager;
 
 
-    /**
-     * The overriden onCreate method setting our layout to main activity.
-     * @param savedInstanceState
-     */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_filter);
-        fragmentManager = getSupportFragmentManager();
-
-
-        StringBuilder url = new StringBuilder(getString(R.string.spells));
-        mSpellsJson = new JSONObject();
+        public JSONObject mSpellsJson;
 
 
 
+        private CheckBox classMystic, classTechnomancer, spellLevel0, spellLevel1, spellLevel2, spellLevel3, spellLevel4, spellLevel5, spellLevel6, schoolAbj, schoolConj, schoolDiv, schoolEnch, schoolEvoc, schoolIllu, schoolNec, schoolTran, rangePersonal, rangeTouch, rangeClose, rangeMedium, rangeLong, rangePlanetary, rangeSystem, rangePlane, castStandard, castMinute, castTenMinutes;
+        private boolean classMysticCheck, classTechnomancerCheck, spellLevel0Check, spellLevel1Check, spellLevel2Check, spellLevel3Check, spellLevel4Check, spellLevel5Check, spellLevel6Check, schoolAbjCheck, schoolConjCheck, schoolDivCheck, schoolEnchCheck, schoolEvocCheck, schoolIlluCheck, schoolNecCheck, schoolTranCheck, rangePersonalCheck, rangeTouchCheck, rangeCloseCheck, rangeMediumCheck, rangeLongCheck, rangePlanetaryCheck, rangeSystemCheck, rangePlaneCheck, castStandardCheck, castMinuteCheck, castTenMinutesCheck;
 
-        checkboxInit();
+
+        /**
+         * The overriden onCreate method setting our layout to main activity.
+         *
+         * @param savedInstanceState
+         */
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_filter);
+            fragmentManager = getSupportFragmentManager();
+
+
+            StringBuilder url = new StringBuilder(getString(R.string.spells));
+            mSpellsJson = new JSONObject();
+
+
+            checkboxInit();
 
 //        StringBuilder url = new StringBuilder(getString(R.string.spells));
 //        mSpellsJson = new JSONObject();
 //        new GetSpellsAsyncTask().execute(url.toString());
 
-    }
+        }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu_main, menu);
 
 
-        StringBuilder url = new StringBuilder(getString(R.string.spells));
+            StringBuilder url = new StringBuilder(getString(R.string.spells));
 
-        return true;
-    }
+            return true;
+        }
 
 //    public void onCheckboxClicked(View view) {
 //        // Is the view now checked?
@@ -201,115 +180,116 @@ public class FilterActivity extends AppCompatActivity {
 //        }
 //    }
 
-    /**
-     * On click method for the Search button.
-     *
-     * @param view
-     */
-    public void searchButton(View view) {
-        Intent intent = new Intent(this, SpellListActivity.class);
-        filterIsChecked();
-        intent.putExtra("classMysticCheck", classMysticCheck);
-        intent.putExtra("classTechnomancerCheck", classTechnomancerCheck);
-        intent.putExtra("spellLevel0Check", spellLevel0Check);
-        intent.putExtra("spellLevel1Check", spellLevel1Check);
-        intent.putExtra("spellLevel2Check", spellLevel2Check);
-        intent.putExtra("spellLevel3Check", spellLevel3Check);
-        intent.putExtra("spellLevel4Check", spellLevel4Check);
-        intent.putExtra("spellLevel5Check", spellLevel5Check);
-        intent.putExtra("spellLevel6Check", spellLevel6Check);
-        intent.putExtra("schoolAbjCheck", schoolAbjCheck);
-        intent.putExtra("schoolConjCheck", schoolConjCheck);
-        intent.putExtra("schoolDivCheck", schoolDivCheck);
-        intent.putExtra("schoolEnchCheck", schoolEnchCheck);
-        intent.putExtra("schoolEvocCheck", schoolEvocCheck);
-        intent.putExtra("schoolIlluCheck", schoolIlluCheck);
-        intent.putExtra("schoolNecCheck", schoolNecCheck);
-        intent.putExtra("schoolTranCheck", schoolTranCheck);
-        intent.putExtra("rangePersonalCheck", rangePersonalCheck);
-        intent.putExtra("rangeTouchCheck", rangeTouchCheck);
-        intent.putExtra("rangeCloseCheck", rangeCloseCheck);
-        intent.putExtra("rangeMediumCheck", rangeMediumCheck);
-        intent.putExtra("rangeLongCheck", rangeLongCheck);
-        intent.putExtra("rangePlanetaryCheck", rangePlanetaryCheck);
-        intent.putExtra("rangeSystemCheck", rangeSystemCheck);
-        intent.putExtra("rangePlaneCheck", rangePlaneCheck);
-        intent.putExtra("castStandardCheck", castStandardCheck);
-        intent.putExtra("castMinuteCheck", castMinuteCheck);
-        intent.putExtra("castTenMinutesCheck", castTenMinutesCheck);
+        /**
+         * On click method for the Search button.
+         *
+         * @param view
+         */
+        public void searchButton(View view) {
+            Intent intent = new Intent(this, SpellListActivity.class);
+            filterIsChecked();
+            intent.putExtra("classMysticCheck", classMysticCheck);
+            intent.putExtra("classTechnomancerCheck", classTechnomancerCheck);
+            intent.putExtra("spellLevel0Check", spellLevel0Check);
+            intent.putExtra("spellLevel1Check", spellLevel1Check);
+            intent.putExtra("spellLevel2Check", spellLevel2Check);
+            intent.putExtra("spellLevel3Check", spellLevel3Check);
+            intent.putExtra("spellLevel4Check", spellLevel4Check);
+            intent.putExtra("spellLevel5Check", spellLevel5Check);
+            intent.putExtra("spellLevel6Check", spellLevel6Check);
+            intent.putExtra("schoolAbjCheck", schoolAbjCheck);
+            intent.putExtra("schoolConjCheck", schoolConjCheck);
+            intent.putExtra("schoolDivCheck", schoolDivCheck);
+            intent.putExtra("schoolEnchCheck", schoolEnchCheck);
+            intent.putExtra("schoolEvocCheck", schoolEvocCheck);
+            intent.putExtra("schoolIlluCheck", schoolIlluCheck);
+            intent.putExtra("schoolNecCheck", schoolNecCheck);
+            intent.putExtra("schoolTranCheck", schoolTranCheck);
+            intent.putExtra("rangePersonalCheck", rangePersonalCheck);
+            intent.putExtra("rangeTouchCheck", rangeTouchCheck);
+            intent.putExtra("rangeCloseCheck", rangeCloseCheck);
+            intent.putExtra("rangeMediumCheck", rangeMediumCheck);
+            intent.putExtra("rangeLongCheck", rangeLongCheck);
+            intent.putExtra("rangePlanetaryCheck", rangePlanetaryCheck);
+            intent.putExtra("rangeSystemCheck", rangeSystemCheck);
+            intent.putExtra("rangePlaneCheck", rangePlaneCheck);
+            intent.putExtra("castStandardCheck", castStandardCheck);
+            intent.putExtra("castMinuteCheck", castMinuteCheck);
+            intent.putExtra("castTenMinutesCheck", castTenMinutesCheck);
 
-        startActivity(intent);
+            startActivity(intent);
+        }
+
+        /**
+         * Helper method to for determining which checkboxes are selected for filters upon Searching
+         */
+        private void filterIsChecked() {
+            classMysticCheck = classMystic.isChecked();
+            classTechnomancerCheck = classTechnomancer.isChecked();
+            spellLevel0Check = spellLevel0.isChecked();
+            spellLevel1Check = spellLevel1.isChecked();
+            spellLevel2Check = spellLevel2.isChecked();
+            spellLevel3Check = spellLevel3.isChecked();
+            spellLevel4Check = spellLevel4.isChecked();
+            spellLevel5Check = spellLevel5.isChecked();
+            spellLevel6Check = spellLevel6.isChecked();
+            schoolAbjCheck = schoolAbj.isChecked();
+            schoolConjCheck = schoolConj.isChecked();
+            schoolDivCheck = schoolDiv.isChecked();
+            schoolEnchCheck = schoolEnch.isChecked();
+            schoolEvocCheck = schoolEvoc.isChecked();
+            schoolIlluCheck = schoolIllu.isChecked();
+            schoolNecCheck = schoolNec.isChecked();
+            schoolTranCheck = schoolTran.isChecked();
+            rangePersonalCheck = rangePersonal.isChecked();
+            rangeTouchCheck = rangeTouch.isChecked();
+            rangeCloseCheck = rangeClose.isChecked();
+            rangeMediumCheck = rangeMedium.isChecked();
+            rangeLongCheck = rangeLong.isChecked();
+            rangePlanetaryCheck = rangePlanetary.isChecked();
+            rangeSystemCheck = rangeSystem.isChecked();
+            rangePlaneCheck = rangePlane.isChecked();
+            castStandardCheck = castStandard.isChecked();
+            castMinuteCheck = castMinute.isChecked();
+            castTenMinutesCheck = castTenMinutes.isChecked();
+        }
+
+        /**
+         * Initializes all the checkboxes on for use with the filters.
+         */
+        private void checkboxInit() {
+            classMystic = findViewById(R.id.checkBox_class_mystic);
+            classTechnomancer = findViewById(R.id.checkBox_class_tech);
+            spellLevel0 = findViewById(R.id.checkBox_level0);
+            spellLevel1 = findViewById(R.id.checkBox_level1);
+            spellLevel2 = findViewById(R.id.checkBox_level2);
+            spellLevel3 = findViewById(R.id.checkBox_level3);
+            spellLevel4 = findViewById(R.id.checkBox_level4);
+            spellLevel5 = findViewById(R.id.checkBox_level5);
+            spellLevel6 = findViewById(R.id.checkBox_level6);
+            schoolAbj = findViewById(R.id.checkBox_abjuration);
+            schoolConj = findViewById(R.id.checkBox_conjuration);
+            schoolDiv = findViewById(R.id.checkBox_divination);
+            schoolEnch = findViewById(R.id.checkBox_enchantment);
+            schoolEvoc = findViewById(R.id.checkBox_evocation);
+            schoolIllu = findViewById(R.id.checkBox_illusion);
+            schoolNec = findViewById(R.id.checkBox_necromancy);
+            schoolTran = findViewById(R.id.checkBox_transmutation);
+            rangePersonal = findViewById(R.id.checkBox_range_personal);
+            rangeTouch = findViewById(R.id.checkBox_range_touch);
+            rangeClose = findViewById(R.id.checkBox_range_close);
+            rangeMedium = findViewById(R.id.checkBox_range_medium);
+            rangeLong = findViewById(R.id.checkBox_range_long);
+            rangePlanetary = findViewById(R.id.checkBox_range_planetary);
+            rangeSystem = findViewById(R.id.checkBox_range_systemwide);
+            rangePlane = findViewById(R.id.checkBox_range_plane);
+            castStandard = findViewById(R.id.checkBox_cast_stdact);
+            castMinute = findViewById(R.id.checkBox_cast_1min);
+            castTenMinutes = findViewById(R.id.checkBox_cast_10min);
+        }
+
+
     }
 
-    /**
-     * Helper method to for determining which checkboxes are selected for filters upon Searching
-     *
-     */
-    private void filterIsChecked() {
-        classMysticCheck = classMystic.isChecked();
-        classTechnomancerCheck = classTechnomancer.isChecked();
-        spellLevel0Check = spellLevel0.isChecked();
-        spellLevel1Check = spellLevel1.isChecked();
-        spellLevel2Check = spellLevel2.isChecked();
-        spellLevel3Check = spellLevel3.isChecked();
-        spellLevel4Check = spellLevel4.isChecked();
-        spellLevel5Check = spellLevel5.isChecked();
-        spellLevel6Check = spellLevel6.isChecked();
-        schoolAbjCheck = schoolAbj.isChecked();
-        schoolConjCheck = schoolConj.isChecked();
-        schoolDivCheck = schoolDiv.isChecked();
-        schoolEnchCheck = schoolEnch.isChecked();
-        schoolEvocCheck = schoolEvoc.isChecked();
-        schoolIlluCheck = schoolIllu.isChecked();
-        schoolNecCheck = schoolNec.isChecked();
-        schoolTranCheck = schoolTran.isChecked();
-        rangePersonalCheck = rangePersonal.isChecked();
-        rangeTouchCheck = rangeTouch.isChecked();
-        rangeCloseCheck = rangeClose.isChecked();
-        rangeMediumCheck = rangeMedium.isChecked();
-        rangeLongCheck = rangeLong.isChecked();
-        rangePlanetaryCheck = rangePlanetary.isChecked();
-        rangeSystemCheck = rangeSystem.isChecked();
-        rangePlaneCheck = rangePlane.isChecked();
-        castStandardCheck = castStandard.isChecked();
-        castMinuteCheck = castMinute.isChecked();
-        castTenMinutesCheck = castTenMinutes.isChecked();
-    }
 
-    /**
-     * Initializes all the checkboxes on for use with the filters.
-     */
-    private void checkboxInit() {
-        classMystic = findViewById(R.id.checkBox_class_mystic);
-        classTechnomancer = findViewById(R.id.checkBox_class_tech);
-        spellLevel0 = findViewById(R.id.checkBox_level0);
-        spellLevel1 = findViewById(R.id.checkBox_level1);
-        spellLevel2 = findViewById(R.id.checkBox_level2);
-        spellLevel3 = findViewById(R.id.checkBox_level3);
-        spellLevel4 = findViewById(R.id.checkBox_level4);
-        spellLevel5 = findViewById(R.id.checkBox_level5);
-        spellLevel6 = findViewById(R.id.checkBox_level6);
-        schoolAbj = findViewById(R.id.checkBox_abjuration);
-        schoolConj = findViewById(R.id.checkBox_conjuration);
-        schoolDiv = findViewById(R.id.checkBox_divination);
-        schoolEnch = findViewById(R.id.checkBox_enchantment);
-        schoolEvoc = findViewById(R.id.checkBox_evocation);
-        schoolIllu = findViewById(R.id.checkBox_illusion);
-        schoolNec = findViewById(R.id.checkBox_necromancy);
-        schoolTran = findViewById(R.id.checkBox_transmutation);
-        rangePersonal = findViewById(R.id.checkBox_range_personal);
-        rangeTouch = findViewById(R.id.checkBox_range_touch);
-        rangeClose = findViewById(R.id.checkBox_range_close);
-        rangeMedium = findViewById(R.id.checkBox_range_medium);
-        rangeLong = findViewById(R.id.checkBox_range_long);
-        rangePlanetary = findViewById(R.id.checkBox_range_planetary);
-        rangeSystem = findViewById(R.id.checkBox_range_systemwide);
-        rangePlane = findViewById(R.id.checkBox_range_plane);
-        castStandard = findViewById(R.id.checkBox_cast_stdact);
-        castMinute = findViewById(R.id.checkBox_cast_1min);
-        castTenMinutes = findViewById(R.id.checkBox_cast_10min);
-    }
-
-
-}
 
