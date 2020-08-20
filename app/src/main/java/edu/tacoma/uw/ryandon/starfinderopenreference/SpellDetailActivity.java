e149a23c532d29a8bc7a78a30e8da6180f65cb46
Nicholas-Babcock <babcon@uw.edu>
2020-08-19 21:26:39 -0700

    changes

--- app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/SpellDetailActivity.java
+++ app/src/main/java/edu/tacoma/uw/ryandon/starfinderopenreference/SpellDetailActivity.java
@@ -30,40 +30,17 @@ public class SpellDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, "Sending this text");
-                sendIntent.setType("text/plain");
 
-                Intent shareIntent = Intent.createChooser(sendIntent, null);
-                startActivity(shareIntent);
 
-            }
-        });
-
-        // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        // savedInstanceState is non-null when there is fragment state
-        // saved from previous configurations of this activity
-        // (e.g. when rotating the screen from portrait to landscape).
-        // In this case, the fragment will automatically be re-added
-        // to its container so we don"t need to manually add it.
-        // For more information, see the Fragments API guide at:
-        //
-        // http://developer.android.com/guide/components/fragments.html
-        //
+
         if (savedInstanceState == null) {
-            // Create the detail fragment and add it to the activity
-            // using a fragment transaction.
+
             Bundle arguments = new Bundle();
             arguments.putSerializable(SpellDetailFragment.ARG_ITEM_ID,
                     getIntent().getSerializableExtra(SpellDetailFragment.ARG_ITEM_ID));
@@ -80,13 +57,6 @@ public class SpellDetailActivity extends AppCompatActivity {
         int id = item.getItemId();
         if (id == android.R.id.home) {
 
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-            //navigateUpTo(new Intent(this, SpellListActivity.class));
             onBackPressed();
             return true;
         }
